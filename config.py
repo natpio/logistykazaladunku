9ea8c01adc63248944f9e87b0138195f78cb552a
natpio <252204917+natpio@users.noreply.github.com>
2026-04-09 16:46:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ UKLADY_LISTA = [
     "🟰 Piętrowany: Dół / Góra"
 ]
 
+# Kategorie sprzętu z Twojej kartki
 KATEGORIE_SPRZETU = [
     "Dioda", "Kablarki", "TV", "Procesory", "Rozdzielnie", "Monitory", "Głośniki", 
     "Wzmacniacze", "Lampy", "Krata", "Drabiny", "Rusztowanie", "Szary plastik. Box", 
-    "Niebieski Plastik box", "Kartony", "Statywy", "LAN", "Narzędziówka", "PC", 
-    "Gravity", "Paleta", "TRAP"
+    "Niebieski Plastik box", "Kartony", "Statywy", "LAN", "Narzędziówka", "PC", "Gravity", "Paleta", "TRAP"
 ]
